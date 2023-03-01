7dc2f5d4f81f5cd43030214b95bb64f13bfbe341
KewWM <weiming050503@gmail.com>
2023-03-01 14:00:36 +0800

    Day1 + 2

---
+++ Day2_977_SquaresofaSortedArray.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Day2_977_SquaresofaSortedArray
+{
+    public int[] sortedSquares(int[] nums)
+    {
+        for(int i = 0; i < nums.length; i++)
+        {
+            nums[i] = nums[i] * nums[i];
+        }
+
+        Arrays.sort(nums);
+        return nums;
+    }
+}
