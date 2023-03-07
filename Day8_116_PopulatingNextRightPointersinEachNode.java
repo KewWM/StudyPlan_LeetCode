3198cc2b43792f605e4ba765eff2dd9ddf703bd0
KewWM <weiming050503@gmail.com>
2023-03-07 18:21:17 +0800

    Day8

---
+++ Day8_116_PopulatingNextRightPointersinEachNode.java
@@ -0,0 +1,41 @@
+public class Day8_116_PopulatingNextRightPointersinEachNode
+{
+    // Definition for a Node.
+    class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+        public Node connect(Node root)
+        {
+            if(root == null)
+                return null;
+            Node left = root.left;
+            Node right = root.right;
+            while(left != null)
+            {
+                left.next = right;
+                left = left.right;
+                right = right.left;
+            }
+            connect(root.left);
+            connect(root.right);
+            return root;
+        }
+}
