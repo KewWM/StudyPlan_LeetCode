10fc044b786f776a0340749598067f7cb886e16a
KewWM <weiming050503@gmail.com>
2023-03-24 16:11:14 +0800

    Day10

---
+++ LeetCode75/Day10_509_FibonacciNumber.java
@@ -0,0 +1,11 @@
+package LeetCode75;
+
+public class Day10_509_FibonacciNumber
+{
+    public int fib(int n)
+    {
+        if(n == 0|| n == 1)
+            return n;
+        return fib(n-1) + fib(n-2);
+    }
+}
